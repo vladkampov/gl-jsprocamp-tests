3a098d6a2a622376a63f95849eaee9fb2b03c25b
vladkampov <vlad@geeny.io>
2018-01-25 19:01:33 +0200

    Add lesson 2 tests schema

---
+++ lesson2.test.js
@@ -0,0 +1,27 @@
+import lessonn2 from '../src/lesson2';
+
+describe('sum function', () => {
+  test('sum works good', () => {
+    
+  });
+});
+
+
+describe('sumAll function', () => {
+  test('sumAll works good', () => {
+
+  });
+});
+
+
+describe('pow function', () => {
+  test('pow works good', () => {
+
+  });
+});
+
+describe('random function', () => {
+  test('random works good', () => {
+
+  });
+});
